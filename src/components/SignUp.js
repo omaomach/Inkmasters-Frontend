5bca4c2f85b0a3a9b6b23bd43dd2c2b36bb96aeb
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-12-06 07:45:44 +0300

    created sign-up component skeleton

---
+++ src/components/SignUp.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function SignUp() {
+
+    return (
+        <div className="main-parent">
+            <div className="left">
+                <div className="awesome-image">
+
+                </div>
+                <div className="art-text">
+
+                </div>
+            </div>
+            <div className="right">
+                <div className="sign-up-header">
+
+                </div>
+                <div className="sign-up-components">
+
+                </div>
+                <div className="login-text">
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
